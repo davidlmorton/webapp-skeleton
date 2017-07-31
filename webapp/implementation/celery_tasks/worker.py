673d6f4c0008332d8ade74a7e9a9714291c68932
David Morton <davidlmorton@gmail.com>
2017-07-31 02:09:56 +0000

    Log exceptions in celery task

--- webapp/implementation/celery_tasks/worker.py
+++ webapp/implementation/celery_tasks/worker.py
@@ -14,4 +14,13 @@ LOG = nicer_logging.getLogger(__name__)
 @app.task()
 def do_work(numerator=None, denominator=None, wallclock_time=None):
     time.sleep(wallclock_time)  # pretend we're doing something really hard
-    return numerator / denominator
+    try:
+        return numerator / denominator
+    except:
+        LOG.exception("Unexpected exception occured while in 'do_work'",
+                extra={'webapp.celery_task': 'worker.do_work',
+                       'webapp.celery_params': {
+                           'numerator': numerator,
+                           'denominator': denominator,
+                           'wallclock_time': wallclock_time}})
+        raise
